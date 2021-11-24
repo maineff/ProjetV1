264afb8f183f6461f73cc5f8de721accb8ccca6e
Maï-linh Neff <57534078+maineff@users.noreply.github.com>
2021-11-24 15:48:42 +0100

    API ok

--- projetv1/src/App.js
+++ projetv1/src/App.js
@@ -1,89 +1,81 @@
-import './App.css';
-import API_FREQUENTATION from "./API_Frequentation";
+import "./App.css";
 // reactstrap components
-import Grid from '@mui/material/Grid';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import Grid from "@mui/material/Grid";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Component } from "react";
 
 const drawerWidth = 50;
 
-function start() {
-  const apiFrequentation = new API_FREQUENTATION();
-  apiFrequentation
-  .fetch()
-  .then(function (response) {
-    const data = response.data;
-    alert(data[0]);
+class App extends React.Component {
+  state = {
+    loading: true,
+    person: null,
+  };
 
-      // On récupère l'information principal
-      const annee = data.frequentation[0].annee;
+  async componentDidMount() {
+    const url =
+      "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.records[0], loading: false });
+  }
 
-      // Modifier le DOM
-      document.getElementById('annee').innerHTML = annee;
-  })
-  .catch(function(error) {
-    // Affiche une erreur
-    console.error(error);
-    alert(error)
-  });
-  
-}
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
 
-function App() {
-  start();
-  return (
-  
-    <Box className='body'> 
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
+    }
 
-      <Drawer sx={{width: drawerWidth,}}
-        variant="permanent"
-        anchor="left"
-      >
-        <List className="drawer" >
-          {['OUI', 'NN'].map((text, index) => (
-            <ListItem button key={text}><ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box 
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: '#212427', p: 3 }}
-      > 
-        <Grid container spacing={1} className='firstcard'>
-          <Grid item xs={4}>
-            <div className='simplecard' >prix moyen d'une séance
-          
-            
-            </div>
+    return (
+      <Box className="body">
+        <Drawer sx={{ width: drawerWidth }} variant="permanent" anchor="left">
+          <List className="drawer">
+            {["OUI", "NN"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: "#212427", p: 3 }}>
+          <Grid container spacing={1} className="firstcard">
+            <Grid item xs={4}>
+              <div className="simplecard">
+                prix moyen d'une séance :
+                <div>{this.state.person.fields.entrees_millions}</div>
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="simplecard">séance du jour</div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="simplecard">Membre</div>
+            </Grid>
+            <Grid item xs={8}>
+              <div className="graphcard">Nombre de visiteur</div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="graphcard">Style</div>
+            </Grid>
+            <Grid item xs={7}>
+              <div className="graphcard">Genre/Age des visiteurs</div>
+            </Grid>
+            <Grid item xs={5}>
+              <div className="graphcard">Origine des films</div>
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <div className='simplecard'>séance du jour</div>
-          </Grid>
-          <Grid item xs={4}>
-            <div className='simplecard' >Membre</div>
-          </Grid>
-          <Grid item xs={8}>
-            <div className='graphcard'>Nombre de visiteur</div>
-          </Grid>
-          <Grid item xs={4}>
-            <div className='graphcard'>Style</div>
-          </Grid>
-          <Grid item xs={7}>
-            <div className='graphcard'>Genre/Age des visiteurs</div>
-          </Grid>
-          <Grid item xs={5}>
-            <div className='graphcard'>Origine des films</div>
-          </Grid>
-        </Grid> 
-      </Box> 
-    </Box> 
-   
-  );
+        </Box>
+      </Box>
+    );
+  }
 }
 
 export default App;
