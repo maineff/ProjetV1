6b82a9fa74d2ebb0cc892e555131d919f784b67a
Maï-linh Neff <57534078+maineff@users.noreply.github.com>
2021-11-25 17:43:23 +0100

    graph sans css avec 1 donnée api

--- projetv1/src/App.js
+++ projetv1/src/App.js
@@ -7,22 +7,39 @@ import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import { Component } from "react";
+import { Bar } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 const drawerWidth = 50;
+
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "couleur pref en france",
+      data: [43, 19, 3, 5, 2, 3],
+    },
+  ],
+};
 
 class App extends React.Component {
   state = {
     loading: true,
-    person: null,
+    info: null,
   };
 
   async componentDidMount() {
     const url =
-      "https://data.culture.gouv.fr/api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee";
+      "https://data.culture.gouv.fr//api/records/1.0/search/?dataset=frequentation-dans-les-salles-de-cinema&q=&facet=annee&refine.annee=2020";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.records[0], loading: false });
+    this.setState({ info: data.records[0], loading: false });
+  }
+  param() {
+    data.datasets[0].data[0] = parseInt(
+      this.state.info.fields.entrees_millions
+    );
+    data.labels[0] = this.state.info.fields.annee;
   }
 
   render() {
@@ -30,15 +47,16 @@ class App extends React.Component {
       return <div>loading...</div>;
     }
 
-    if (!this.state.person) {
-      return <div>didn't get a person</div>;
+    if (!this.state.info) {
+      return <div>ERREUR</div>;
     }
+    this.param();
 
     return (
       <Box className="body">
         <Drawer sx={{ width: drawerWidth }} variant="permanent" anchor="left">
           <List className="drawer">
-            {["OUI", "NN"].map((text, index) => (
+            {["HOME", "SEARCH"].map((text, index) => (
               <ListItem button key={text}>
                 <ListItemText primary={text} />
               </ListItem>
@@ -49,27 +67,60 @@ class App extends React.Component {
           <Grid container spacing={1} className="firstcard">
             <Grid item xs={4}>
               <div className="simplecard">
-                prix moyen d'une séance :
-                <div>{this.state.person.fields.entrees_millions}</div>
+                Nombre d'entrées en millions
+                <div>{this.state.info.fields.entrees_millions}</div>
+                en <div>{this.state.info.fields.annee}</div>
               </div>
             </Grid>
             <Grid item xs={4}>
-              <div className="simplecard">séance du jour</div>
+              <div className="simplecard">
+                Nombre de séances
+                <div>{this.state.info.fields.seances_milliers}</div>
+              </div>
             </Grid>
             <Grid item xs={4}>
-              <div className="simplecard">Membre</div>
+              <div className="simplecard">
+                Recette moyenne par personne
+                <div>
+                  {this.state.info.fields.recette_moyenne_par_entree_eur} euros
+                </div>
+              </div>
             </Grid>
             <Grid item xs={8}>
-              <div className="graphcard">Nombre de visiteur</div>
+              <div className="graphcard">
+                Nombre d'entrées
+                {/* a voir si c'est utile
+                <iframe
+                  src="https://data.culture.gouv.fr/chart/embed/?dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJsaW5lIiwiZnVuYyI6IlNVTSIsInlBeGlzIjoiZW50cmVlc19taWxsaW9ucyIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6IiMwMDAwODAifV0sInhBeGlzIjoiYW5uZWUiLCJtYXhwb2ludHMiOjUwLCJzb3J0IjoiIiwiY29uZmlnIjp7ImRhdGFzZXQiOiJmcmVxdWVudGF0aW9uLWRhbnMtbGVzLXNhbGxlcy1kZS1jaW5lbWEiLCJvcHRpb25zIjp7ImRpc2p1bmN0aXZlLmFubmVlIjp0cnVlfX19XSwidGltZXNjYWxlIjoiIiwiZGlzcGxheUxlZ2VuZCI6dHJ1ZSwiYWxpZ25Nb250aCI6dHJ1ZX0%3D&static=false&datasetcard=false"
+                  width="400"
+                  height="300"
+                  frameborder="0"
+                ></iframe>*/}
+              </div>
             </Grid>
             <Grid item xs={4}>
-              <div className="graphcard">Style</div>
+              <div className="graphcard">
+                Recette
+                <Bar className="bar" data={data} />
+                <div>
+                  {this.state.info.fields.recette_guichet_meur_courants}{" "}
+                  millions d'euros
+                </div>
+              </div>
             </Grid>
             <Grid item xs={7}>
               <div className="graphcard">Genre/Age des visiteurs</div>
             </Grid>
             <Grid item xs={5}>
-              <div className="graphcard">Origine des films</div>
+              <div className="graphcard">
+                {/*Géolocalisation des cinémas en France
+                <iframe
+                  src="https://data.culture.gouv.fr/explore/embed/dataset/etablissements-cinematographiques/analyze/?dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQ09VTlQiLCJzY2llbnRpZmljRGlzcGxheSI6dHJ1ZSwiY29sb3IiOiIjOGRhMGNiIn1dLCJ4QXhpcyI6InJlZ2lvbl9hZG1pbmlzdHJhdGl2ZSIsIm1heHBvaW50cyI6NTAsInNvcnQiOiIiLCJjb25maWciOnsiZGF0YXNldCI6ImV0YWJsaXNzZW1lbnRzLWNpbmVtYXRvZ3JhcGhpcXVlcyIsIm9wdGlvbnMiOnt9fX1dLCJ0aW1lc2NhbGUiOiIiLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlfQ%3D%3D&static=false&datasetcard=false"
+                  width="400"
+                  height="300"
+                  frameborder="0"
+                ></iframe>*/}
+              </div>
             </Grid>
           </Grid>
         </Box>
